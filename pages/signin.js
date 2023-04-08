8993a17a5c6223f857763a934987c6fe679b2625
prathmeshchhabra <p.chhabra2002@gmail.com>
2023-04-09 01:57:19 +0530

    new pages added

---
+++ pages/signin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div className="text-2xl">Sign In Page</div>;
+};
+
+export default SignIn;
---
+++ pages/signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div className="text-2xl">Sign Up Page</div>;
+};
+
+export default SignUp;
