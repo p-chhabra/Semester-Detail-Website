88929bea34720e175ab875a0e5bf93978edf520d
prathmeshchhabra <p.chhabra2002@gmail.com>
2023-04-09 13:03:50 +0530

    files added/deleted

--- pages/signin.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignIn = () => {
-  return <div className="text-2xl">Sign In Page</div>;
-};
-
-export default SignIn;
--- pages/signup.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignUp = () => {
-  return <div className="text-2xl">Sign Up Page</div>;
-};
-
-export default SignUp;
