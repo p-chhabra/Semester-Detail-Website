e511bb30790a70ebf065f1d040634a5a6ee2ff59
prathmeshchhabra <p.chhabra2002@gmail.com>
2023-04-10 14:10:42 +0530

    button style changed

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,6 @@ const About = () => {
     <div>
       <h1 className={styles.h1}>Our Team</h1>
       <ul className={styles.body}>
-<<<<<<< HEAD
         <li></li>
         <li>
           <div className={styles.person}>
@@ -22,7 +21,11 @@ const About = () => {
               </div>
             </div>
             <div className={styles.divider}></div>
-            <div className={styles.name}>Prathmesh Chhabra</div>
+            <div className={styles.name}>
+              <a href="https://www.linkedin.com/in/prathmesh-chhabra-51760719b/">
+                Prathmesh Chhabra
+              </a>
+            </div>
           </div>
         </li>
         <li>
@@ -38,7 +41,11 @@ const About = () => {
               </div>
             </div>
             <div className={styles.divider}></div>
-            <div className={styles.name}>Aditya Tiwari</div>
+            <div className={styles.name}>
+              <a href="https://www.linkedin.com/in/aditya-tiwari-438087203/">
+                Aditya Tiwari
+              </a>
+            </div>
           </div>
         </li>
         <li>
@@ -54,7 +61,11 @@ const About = () => {
               </div>
             </div>
             <div className={styles.divider}></div>
-            <div className={styles.name}>Jayant Saini</div>
+            <div className={styles.name}>
+              <a href="https://www.linkedin.com/in/jayant-saini-59803123a/">
+                Jayant Saini
+              </a>
+            </div>
           </div>
         </li>
         <li>
@@ -70,63 +81,16 @@ const About = () => {
               </div>
             </div>
             <div className={styles.divider}></div>
-            <div className={styles.name}>Aaditya Gagneja</div>
+            <div className={styles.name}>
+              <a href="https://www.linkedin.com/in/aaditya-gagneja-b727a0203/">
+                Aaditya Gagneja
+              </a>
+            </div>
           </div>
         </li>
       </ul>
     </div>
   );
-=======
-      <li>
-      </li>
-      <li>
-      <div className={styles.person}>
-      <div className={styles.container}>
-        <div className={styles.containerinner}>
-          <Image className={styles.circle} src="/im1.jpg" width={500} height={500}></Image>
-        </div>
-      </div>
-      <div className={styles.divider}></div>
-      <div className={styles.name}><a href="https://www.linkedin.com/in/prathmesh-chhabra-51760719b/">Prathmesh Chhabra</a></div>
-    </div>
-      </li>
-      <li>
-      <div className={styles.person}>
-      <div className={styles.container}>
-        <div className={styles.containerinner}>
-          <Image className={styles.circle} src="/im2.webp" width={500} height={500} ></Image>
-        </div>
-      </div>
-      <div className={styles.divider}></div>
-      <div className={styles.name}><a href="https://www.linkedin.com/in/aditya-tiwari-438087203/">Aditya Tiwari</a></div>
-    </div>
-      </li>
-      <li>
-      <div className={styles.person}>
-      <div className={styles.container}>
-        <div className={styles.containerinner}>
-        <Image className={styles.circle} src="/im4.jpg" width={500} height={500}></Image>
-        </div>
-      </div>
-      <div className={styles.divider}></div>
-      <div className={styles.name}><a href="https://www.linkedin.com/in/jayant-saini-59803123a/">Jayant Saini</a></div>
-    </div>
-      </li>
-      <li>
-      <div className={styles.person}>
-      <div className={styles.container}>
-        <div className={styles.containerinner}>
-        <Image className={styles.circle} src="/im3.jpg" width={500} height={500}></Image>
-        </div>
-      </div>
-      <div className={styles.divider}></div>
-      <div className={styles.name}><a href="https://www.linkedin.com/in/aaditya-gagneja-b727a0203/">Aaditya Gagneja</a></div>
-    </div>
-      </li>
-    </ul>
-    </div>
-  )
->>>>>>> 9e93f333a0613b6c9a49404d2861512c2f4d4c1c
 };
 
 export default About;
