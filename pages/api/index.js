cdff4ed4110e6b8dd4ec68f841a9ffd9a9d1ce6f
prathmeshchhabra <p.chhabra2002@gmail.com>
2023-06-25 01:05:25 +0530

    comment section added

--- pages/api/index.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import dbConnect from "../../lib/mongodb";
-
-export default async function handler(req, res) {
-  const params = req.params;
-  console.log(params);
-  await dbConnect();
-  res.status(200).json({ name: "John Doe" });
-}
